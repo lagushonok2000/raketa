58af59178a0908a482033dee8e820e745f45fda2
lagushonok2000 <vorobjeva.an4stasiy@yandex.ru>
2024-03-27 19:33:23 +0300

    second

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,9 +4,15 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform _player;
     private Vector3 _newPosition;
+    private Transform _camera;
+
+    private void Start()
+    {
+        _camera = Camera.main.transform;
+    }
     private void Update()
     {
-        _newPosition = new Vector3(_player.position.x, _player.position.y, transform.position.z);
-        transform.position = _newPosition;
+        _newPosition = new Vector3(_player.position.x, _player.position.y, _camera.position.z);
+        _camera.position = _newPosition;
     }
 }
